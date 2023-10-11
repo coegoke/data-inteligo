c00d5209489b1a72c4f4396680e13869e8453307
coegoke <49462578+coegoke@users.noreply.github.com>
2023-10-11 21:53:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,74 @@
-import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
-import streamlit as st  # pip install streamlit
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-import nltk
-import unicodedata
-import re
-from nltk.util import ngrams
-from nltk.corpus import stopwords
-st.set_page_config(page_title="Review Dashboard", page_icon=":shark:", layout="wide")
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+import plotly.express as px
+import streamlit as st
+st.set_page_config(
+    page_title="Twitter Sentiment Analyzer", page_icon="📊", layout="wide"
+)
+st.title(":bar_chart: Review Sentiment Dashboard")
+def get_top_n_gram(tweet_df, ngram_range, n=10):
+    corpus = tweet_df["Text"]
+    vectorizer = CountVectorizer(
+        analyzer="word", ngram_range=ngram_range
+    )
+    X = vectorizer.fit_transform(corpus.astype(str).values)
+    words = vectorizer.get_feature_names_out()
+    words_count = np.ravel(X.sum(axis=0))
+    df = pd.DataFrame(zip(words, words_count))
+    df.columns = ["words", "counts"]
+    df = df.sort_values(by="counts", ascending=False).head(n)
+    df["words"] = df["words"].str.title()
+    return df
 
+def plot_n_gram(n_gram_df, title, color="#54A24B"):
+    fig = px.bar(
+        x=n_gram_df.counts,
+        y=n_gram_df.words,
+        title="<b>{}</b>".format(title),
+        text_auto=True,
+    )
+    fig.update_layout(plot_bgcolor="white")
+    fig.update_xaxes(title=None)
+    fig.update_yaxes(autorange="reversed", title=None)
+    fig.update_traces(hovertemplate="<b>%{y}</b><br>Count=%{x}", marker_color=color)
+    return fig
+
+def plot_sentiment(tweet_df):
+    sentiment_count = tweet_df["status"].value_counts()
+    fig = px.pie(
+        values=sentiment_count.values,
+        names=sentiment_count.index,
+        hole=0.3,
+        title="<b>Sentiment Distribution</b>",
+        color=sentiment_count.index,
+        color_discrete_map={"Positive": "#1F77B4", "Negative": "#FF7F0E"},
+    )
+    fig.update_traces(
+        textposition="inside",
+        texttemplate="%{label}<br>%{value} (%{percent})",
+        hovertemplate="<b>%{label}</b><br>Percentage=%{percent}<br>Count=%{value}",
+    )
+    fig.update_layout(showlegend=False)
+    return fig
+
+
+
+adjust_top_pad = """
+    <style>
+        div.block-container {padding-top:1rem;}
+    </style>
+    """
+st.markdown(adjust_top_pad, unsafe_allow_html=True)
 df = pd.read_csv('data_deploy.csv')
-
-# ---- SIDEBAR ----
-st.sidebar.header("Please Filter Here:")
-sumber = st.sidebar.multiselect(
+st.header("Please Filter Here:")
+sumber = st.multiselect(
     "Select the sumber:",
     options=df["sumber"].unique(),
     default=['twitter','playstore']
 )
 
-status = st.sidebar.multiselect(
+status = st.multiselect(
     "Select the status:",
     options=df["status"].unique(),
     default=df["status"].unique()
@@ -29,79 +77,31 @@ status = st.sidebar.multiselect(
 df_selection = df.query(
     "sumber == @sumber & status ==@status"
 )
-
-# Join the different processed titles together.
-# ---- MAINPAGE ----
-st.title(":bar_chart: Review Dashboard")
-st.markdown("##")
-st.set_option('deprecation.showPyplotGlobalUse', False)
-value_counts = df_selection['Keywords_kategori'].value_counts().head().reset_index()
+def make_dashboard(tweet_df, bar_color, wc_color):
+        # first row
+        col1, col2, col3 = st.columns([25, 40, 35])
+        with col1:
+            sentiment_plot = plot_sentiment(tweet_df)
+            sentiment_plot.update_layout(title_x=0.5,title_font=dict(size=24))
+            st.plotly_chart(sentiment_plot, theme=None, use_container_width=True)
+        with col2:
+            top_unigram = get_top_n_gram(tweet_df, ngram_range=(1, 1), n=10)
+            unigram_plot = plot_n_gram(
+                top_unigram, title="Top 10 Occuring Words", color=bar_color
+            )
+            unigram_plot.update_layout(title_font=dict(size=24))
+            st.plotly_chart(unigram_plot, theme=None, use_container_width=True)
+        with col3:
+            top_bigram = get_top_n_gram(tweet_df, ngram_range=(2, 2), n=10)
+            bigram_plot = plot_n_gram(
+                top_bigram, title="Top 10 Occuring Bigrams", color=bar_color
+            )
+            bigram_plot.update_layout(title_font=dict(size=24))
+            st.plotly_chart(bigram_plot, theme=None, use_container_width=True)
+make_dashboard(df_selection, bar_color="#54A24B", wc_color="Greens")
+value_counts = df_selection['Keywords_kategori'].value_counts().head(8).reset_index()
 value_counts.columns = ['Category', 'Count']
 value_counts = value_counts.sort_values(by='Count', ascending=True)
-fig_topic = px.bar(value_counts, y='Category', x='Count', orientation='h', title='Grafik Batang Vertikal Berdasarkan Value Counts')
-long_string = ','.join(list(df_selection['Text'].astype(str).values))
-wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
-WordCloud = wordcloud.generate(long_string)
-# plt.imshow(WordCloud)
-# st.pyplot()
- # Visualize the word cloud
-# Convert WordCloud to a Matplotlib figure
-wordcloud_matplotlib = plt.figure()
-plt.imshow(WordCloud)
-plt.axis("off")  # Turn off axis
-plt.title("Word Cloud")
+fig_topic = px.bar(value_counts, y='Category', x='Count', orientation='h', title='top Topic Modelling')
+st.plotly_chart(fig_topic, use_container_width=True)
 
-left_column, right_column = st.columns([60,40])
-left_column.plotly_chart(fig_topic, use_container_width=True)
-right_column.pyplot(wordcloud_matplotlib, use_container_width=True)
-
-def basic_clean(text):
-  text = (unicodedata.normalize('NFKD', text)
-    .encode('ascii', 'ignore')
-    .decode('utf-8', 'ignore')
-    .lower())
-  words = re.sub(r'[^\w\s]', '', text).split()
-  return words
-words = basic_clean(''.join(str(df_selection['Text'].tolist())))
-onegrams = (pd.Series(nltk.ngrams(words,1)).value_counts()).head(10)
-bigrams = (pd.Series(nltk.ngrams(words,2)).value_counts()).head(10)
-trigrams = (pd.Series(nltk.ngrams(words,3)).value_counts()).head(10)
-
-df_grams1 = pd.Series(onegrams).reset_index()
-df_grams1['Pasangan Kata'] = df_grams1['index'].apply(lambda x: ' '.join(x))  # Mengubah tuple menjadi string
-# Mengurutkan DataFrame berdasarkan frekuensi
-df_grams1 = df_grams1.rename(columns={0: 'Jumlah'})
-df_grams1 = df_grams1.sort_values(by='Jumlah', ascending=False)
-# Membuat bar chart interaktif menggunakan Plotly
-fig_onegrams = px.bar(df_grams1, x='Jumlah', y='Pasangan Kata', orientation='h', 
-             title='one word Phrases')
-fig_onegrams.update_traces(marker_color='skyblue')
-fig_onegrams.update_layout(xaxis_title='Jumlah', yaxis_title='One word Phrases', yaxis_categoryorder='total ascending')
-
-df_grams2 = pd.Series(bigrams).reset_index()
-df_grams2['Pasangan Kata'] = df_grams2['index'].apply(lambda x: ' '.join(x))  # Mengubah tuple menjadi string
-# Mengurutkan DataFrame berdasarkan frekuensi
-df_grams2 = df_grams2.rename(columns={0: 'Jumlah'})
-df_grams2 = df_grams2.sort_values(by='Jumlah', ascending=False)
-# Membuat bar chart interaktif menggunakan Plotly
-fig_bigrams = px.bar(df_grams2, x='Jumlah', y='Pasangan Kata', orientation='h', 
-             title='Two word Phrases')
-fig_bigrams.update_traces(marker_color='skyblue')
-fig_bigrams.update_layout(xaxis_title='Jumlah', yaxis_title='Two word Phrases', yaxis_categoryorder='total ascending')
-
-df_grams3 = pd.Series(trigrams).reset_index()
-df_grams3['Pasangan Kata'] = df_grams3['index'].apply(lambda x: ' '.join(x))  # Mengubah tuple menjadi string
-# Mengurutkan DataFrame berdasarkan frekuensi
-df_grams3 = df_grams3.rename(columns={0: 'Jumlah'})
-df_grams3 = df_grams3.sort_values(by='Jumlah', ascending=False)
-# Membuat bar chart interaktif menggunakan Plotly
-fig_trigrams = px.bar(df_grams3, x='Jumlah', y='Pasangan Kata', orientation='h', 
-             title='Three word Phrases')
-fig_trigrams.update_traces(marker_color='skyblue')
-fig_trigrams.update_layout(xaxis_title='Jumlah', yaxis_title='Three word Phrases', yaxis_categoryorder='total ascending')
-
-
-left_column2,center_column2, right_column2 = st.columns(3)
-left_column2.plotly_chart(fig_onegrams, use_container_width=True)
-center_column2.plotly_chart(fig_bigrams, use_container_width=True)
-right_column2.plotly_chart(fig_trigrams, use_container_width=True)
